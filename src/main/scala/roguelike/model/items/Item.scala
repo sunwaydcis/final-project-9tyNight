faa85bf982f9f5d35045c6b57d426e09e9c2f34d
9tyNight <ziadanka2003@gmail.com>
2024-12-26 17:38:41 +0800

    Create Item.scala

---
+++ src/main/scala/roguelike/model/items/Item.scala
@@ -0,0 +1,13 @@
+package roguelike.model.items
+
+import roguelike.model.characters.Entity
+
+trait Item {
+  val name: String
+  val description: String
+  val symbol: Char
+  var x: Int = 0
+  var y: Int = 0
+
+  def use(target: Entity): Unit 
+}
