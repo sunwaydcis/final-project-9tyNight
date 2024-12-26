0df1b8df3cc7956768a678309c8ba36ed3b2c5d3
9tyNight <ziadanka2003@gmail.com>
2024-12-26 17:28:34 +0800

    Delete Node.scala

--- src/main/scala/roguelike/dungeon/Node.scala
+++
@@ -1,9 +0,0 @@
-package roguelike.dungeon
-
-import scala.math.sqrt
-
-case class Node(x: Int, y: Int, var gCost: Int = 0, var hCost: Int = 0, var parent: Option[Node] = None):
-  def fCost: Int = gCost + hCost
-  
-  def calculateHCost(endNode: Node): Unit =
-    hCost = (Math.abs(x - endNode.x) + Math.abs(y - endNode.y))
