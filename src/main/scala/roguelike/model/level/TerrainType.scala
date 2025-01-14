fd769dc808ed095811ece7fae5be3ca1b80063c9
9tyNight <ziadanka2003@gmail.com>
2025-01-14 18:45:21 +0800

    Update TerrainType.scala

--- src/main/scala/roguelike/model/level/TerrainType.scala
+++ src/main/scala/roguelike/model/level/TerrainType.scala
@@ -1,5 +1,16 @@
 package roguelike.model.level
 
 enum TerrainType {
-  case Wall, Floor, Stairs, Door
+  case StairsUp
+  case StairsDown
+  case Floor
+  case Wall
+  case Floor2
+  case Wall2
+  case Floor3
+  case Wall3
+  case CorpseBones1
+  case CorpseBones2
+  case BloodSpatter1
+  case BloodSpatter2
 }
