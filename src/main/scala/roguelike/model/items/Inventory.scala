2778bfac08cc03aeb8e37eaf52138f9801de39bf
9tyNight <ziadanka2003@gmail.com>
2025-01-14 18:48:40 +0800

    Update Inventory.scala

--- src/main/scala/roguelike/model/items/Inventory.scala
+++ src/main/scala/roguelike/model/items/Inventory.scala
@@ -2,27 +2,58 @@ package roguelike.model.items
 
 import scala.collection.mutable.ListBuffer
 import roguelike.model.characters.Entity
+import roguelike.model.Game
 
-class Inventory {
-  private val items: ListBuffer[Item] = ListBuffer.empty[Item]
+case class Inventory(
+                      private val items: ListBuffer[Item] = ListBuffer.empty[Item],
+                      capacity: Int = 36
+                    ) {
 
-  def addItem(item: Item): Unit = {
-    items += item
-    println(s"Added ${item.name} to inventory.")
+  def getItems(): List[Item] = items.toList
+
+  def addItem(item: Item): Boolean = {
+    if (items.size < capacity) {
+      items += item
+      println(s"Added ${item.name} to inventory.")
+      true
+    } else {
+      println("Inventory is full!")
+      false
+    }
   }
 
   def removeItem(item: Item): Unit = {
     items -= item
   }
 
-  def getItems(): List[Item] = items.toList
-
-  // Use an item on the target
-  def useItem(item: Item, target: Entity): Unit = {
-    if (items.contains(item)) {
-      item.use(target)
+  def useItem(item: Item, user: Entity, game: Game): Unit = {
+    item.use(user)
+    println(s"${user.name} used ${item.name}.")
+    
+    if (!(item.isInstanceOf[Weapon] ||
+      item.isInstanceOf[Pendant] ||
+      item.isInstanceOf[Ring] ||
+      item.isInstanceOf[Helmet] ||
+      item.isInstanceOf[Chestplate] ||
+      item.isInstanceOf[Shield])) {
       removeItem(item)
-      println(s"Used ${item.name} on ${target.name}")
+    }
+  }
+
+  def insertItemAt(item: Item, index: Int): Unit = {
+    println(
+      s"Attempting to insert ${item.name} at index $index. Inventory size: ${items.size}, capacity: $capacity"
+    )
+    if (index >= 0 && index <= items.size && items.size < capacity) {
+      items -= item
+      items.insert(index, item)
+      println(
+        s"Successfully added ${item.name} to inventory at index $index. New inventory size: ${items.size}"
+      )
+    } else {
+      println(
+        s"Cannot insert item at the specified index. Index: $index, Size: ${items.size}, Capacity: $capacity"
+      )
     }
   }
 }
